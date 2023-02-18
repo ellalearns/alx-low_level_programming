3a797c494bff765d42acdf0297957df8658dd2cb
Ella <ellalearnscopy@gmail.com>
2023-02-18 14:22:35 +0100

    kjsdjksd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+
+/**
+ * this
+ * is
+ * a
+ * function
+ * description
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return(0);
 }
