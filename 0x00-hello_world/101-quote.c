0059422e0908687570656974cbe09fbf3fed0347
Ella <ellalearnscopy@gmail.com>
2023-02-21 20:46:59 +0100

    this batch

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * description description
+ * more description
+ * i hate comments 
+ * ghhhh
+*/
+int main(void)
+{
+
+/**
+ * what
+ * the
+ * f
+*/
+printf("nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return
+{
+    1
+}
+
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * description
+*/
+int main(void)
+{
+
+/**
+ * doing something
+*/
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+/**
+ * completely annoying right now
+*/
+return
+{
+    0
+}
+
+}
