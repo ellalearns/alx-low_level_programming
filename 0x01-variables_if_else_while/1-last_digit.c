9df31d23a30689ee09ebc2add3d2870a2e84aca2
Ella <ellalearnscopy@gmail.com>
2023-02-23 20:10:42 +0100

    dssddf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,25 +11,25 @@
 int main(void)
 {
 	int n;
-	int lastDigitOfN;
+	int lastN;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-    
-	lastDigitOfN = n%10;
 
-if (lastDigitOfN > 5)
+	lastN = n % 10;
+
+if (lastN > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, lastDigitOfN);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastN);
 }
-if (lastDigitOfN == 0)
+if (lastN == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, lastDigitOfN);
+printf("Last digit of %d is %d and is 0\n", n, lastN);
 }
-if (lastDigitOfN < 6 && lastDigitOfN != 0)
+if (lastN < 6 && lastN != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigitOfN);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastN);
 }
 	return (0);
 }
