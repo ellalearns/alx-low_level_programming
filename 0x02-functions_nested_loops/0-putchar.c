63a5efd1e3c2537ae819472ea1f45ff4023978f2
Ella <ellalearnscopy@gmail.com>
2023-03-02 21:47:41 +0100

    kjsdjksd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,17 @@
 */
 int _putchar()
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-putchar('\n');
+// putchar('_');
+// putchar('p');
+// putchar('u');
+// putchar('t');
+// putchar('c');
+// putchar('h');
+// putchar('a');
+// putchar('r');
+// putchar('\n');
+
+printf("_putchar\n");
 
 return (0);
 
