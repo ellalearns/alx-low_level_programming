19abbf01d1df15279bff506f7f221e9b9a420769
Ella <ellalearnscopy@gmail.com>
2023-03-18 15:00:36 +0100

    jkdskljs

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,9 +4,10 @@
 
 /**
  * a function description
- * reset_to_98 : updates the value it points to to 98
+ * reset_to_98 - updates the value it points to to 98
  * @n: the pointer it takes in
 */
+
 void reset_to_98(int *n)
 /**
  * can this people just leave me alone
