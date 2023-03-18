0d1e4c3517a242c14c8c2e3dcce9bc5da7c7c63d
Ella <ellalearnscopy@gmail.com>
2023-03-18 14:55:38 +0100

    catching up

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 
 /**
- * reset_to_98: takes a pointer to an int and updates the value it points to to 98
+ * void reset_to_98 : updates the value it points to to 98
  * @n: the pointer it takes in
 */
 void reset_to_98(int *n)
