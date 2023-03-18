6fc7173b025f9ae34aa2398e3651f931dfb1f6c3
Ella <ellalearnscopy@gmail.com>
2023-03-18 15:49:49 +0100

    jkdsjklsd

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string backwards
+ * @s: the string
+*/
+void print_rev(char *s)
+{
+
+int len;
+len = 0;
+
+while (s[len]) {
+    len = len + 1;
+}
+
+for (len = len-1; len >= 0; len--) {
+    printf("%c", s[len]);
+}
+
+}
