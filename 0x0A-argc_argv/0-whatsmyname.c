704b8c93bd262f66961143749848a83b93118684
Ella <ellalearnscopy@gmail.com>
2023-03-20 21:43:55 +0100

    fsfs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main program
+ * @argc: first program param
+ * @argv: second program param
+*/
+int main(int argc, char *argv[])
+{
+
+printf("%s\n", argv[0]);
+return (0);
+
+}
