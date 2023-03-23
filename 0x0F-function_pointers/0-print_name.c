a9f243be9400d04e254bd61df06597ee824eb6c8
Ella <ellalearnscopy@gmail.com>
2023-03-23 02:42:25 +0100

    ifghsr

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: a pointer to a function
+*/
+void print_name(char *name, void (*f)(char*))
+{
+
+f(name);
+
+}
