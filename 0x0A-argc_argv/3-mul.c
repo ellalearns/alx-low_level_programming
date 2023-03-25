296115951b2bdb0e00e6a528a0c830382816f21c
Ella <ellalearnscopy@gmail.com>
2023-03-25 16:41:45 +0100

    dsre

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,6 +2,45 @@
 #include <stdio.h>
 
 /**
+ * myatoi - converts a string to an integer
+ * @s: the string
+ * Return: integer
+*/
+int myatoi(char *s)
+{
+    int result;
+    int digit;
+    int i = 0;
+    int counter = 0;
+    int n = 0;
+    
+    while (s[counter])
+    {
+        counter++;
+    }
+    
+    if (s[0] == '-')
+    {
+        i = 1;
+    }
+    
+    while (s[i])
+    {
+        digit = s[i] - '0';
+        n = n * 10 + digit;
+        i++;
+    }
+    
+    if (s[0] == '-')
+    {
+        return 0 - n;
+    }
+    
+    return n;
+    
+}
+
+/**
  * main - main program
  * @argc: first param
  * @argv: second param
@@ -19,8 +58,8 @@ printf("Error\n");
 return (1);
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
+num1 = myatoi(argv[1]);
+num2 = myatoi(argv[2]);
 
 printf("%d\n", num1 * num2);
 
