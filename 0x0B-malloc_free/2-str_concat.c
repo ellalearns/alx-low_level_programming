278dd689ff415d908a9e19e2a9a8bc658ea94917
Ella <ellalearnscopy@gmail.com>
2023-04-04 22:30:18 +0100

    dweee

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,30 @@
 char *str_concat(char *s1, char *s2)
 {
 char *newString;
-int i, len, counter;
-i = len = counter = 0;
-if (s1 != NULL)
-{
-while (s1[len])
-len++;
-}
-if (s2 != NULL)
-{
-while (s2[len])
-len++;
-}
-newString = malloc(sizeof(char) * (len + 1));
+int len1, len2, counter;
+len1 = len2 = counter = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
+newString = malloc(sizeof(char) * (len1 + len2 + 1));
 if (newString == NULL)
-return(NULL);
-while (s1[i])
+return (NULL);
+len1 = len2 = 0;
+while (s1[len1])
 {
-newString[i] = s1[i];
-i++;
+newString[len1] = s1[len1];
+len1++;
 }
-i = i + 1;
-while (s2[counter])
+while (s2[len2])
 {
-newString[i] = s2[counter];
-counter++;
-i++;
+newString[len1] = s2[len2];
+len1++;
+len2++;
 }
 return (newString);
 }
