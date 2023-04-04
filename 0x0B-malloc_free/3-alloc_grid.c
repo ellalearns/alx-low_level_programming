57280d5aa31d390c35dade03a00b7dc00bb404cb
Ella <ellalearnscopy@gmail.com>
2023-04-04 22:42:03 +0100

    dweeww

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - func
+ * @width: param
+ * @height: param
+ * Return: int
+*/
+int **alloc_grid(int width, int height)
+{
+int *grid;
+int len;
+
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+
+grid = malloc(sizeof(int) * (width * height));
+
+if (grid == NULL)
+{
+return (NULL);
+}
+
+len = 0;
+
+while (grid[len])
+{
+grid[len] = 0;
+len++;
+}
+
+return grid;
+}
