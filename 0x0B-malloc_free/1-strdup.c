b66cfcf25a62b12140ba129dc9440bed3dca18f7
Ella <ellalearnscopy@gmail.com>
2023-04-04 21:54:45 +0100

    dwer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - func
+ * @str: param
+ * Return: char
+*/
+char *_strdup(char *str)
+{
+char *newStr;
+int i;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+newStr = malloc(sizeof(str));
+
+if (newStr == NULL)
+{
+return (NULL);
+}
+
+i = 0;
+
+while (newStr[i])
+{
+newStr[i] = str[i];
+i++;
+}
+
+return (newStr);
+
+}
