d783c1ed92d0ece305c6ce60498a8c9810d2fa5f
Ella <ellalearnscopy@gmail.com>
2023-04-05 07:17:07 +0100

    sdewwe

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * init_dog - init dog
+ * @d: first param
+ * @name: second param
+ * @age: third param
+ * @owner: fourth param
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
