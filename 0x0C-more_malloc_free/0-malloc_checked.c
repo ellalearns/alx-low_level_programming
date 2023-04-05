5c38e177fa0a9224461fe817390ade0f69741da7
Ella <ellalearnscopy@gmail.com>
2023-04-05 08:13:40 +0100

    sidofijeo

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - func
+ * @b: param
+*/
+void *malloc_checked(unsigned int b)
+{
+void *mallocPointer;
+
+mallocPointer = malloc(b);
+
+if (mallocPointer == NULL)
+{
+exit(98);
+}
+
+return mallocPointer;
+}
