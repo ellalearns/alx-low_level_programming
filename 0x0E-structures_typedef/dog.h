c32c866763b673f208069a5ff9550abe8e63ffcd
Ella <ellalearnscopy@gmail.com>
2023-04-05 07:09:27 +0100

    sdse

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+
+/**
+ * struct dog - dog structure
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ * 
+ * Description: task one
+*/
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
