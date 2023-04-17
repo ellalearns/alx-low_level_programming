a90f1943fbe41fe56b5d16d99b480b009c88a30a
Ella <ellalearnscopy@gmail.com>
2023-04-17 15:27:45 +0100

    dsdewew

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,24 +10,24 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list infargs;
-    unsigned int i;
-    unsigned int j;
+va_list infargs;
+unsigned int i;
+unsigned int j;
 
-    if (n == 0)
-    {
-        return (0);
-    }
+if (n == 0)
+{
+return (0);
+}
 
-    j = 0;
+j = 0;
 
-    va_start(infargs, n);
+va_start(infargs, n);
 
-    for (i = 0; i < n; i++)
-    {
-        j += va_arg(infargs, int);
-    }
+for (i = 0; i < n; i++)
+{
+j += va_arg(infargs, int);
+}
 
-    return (j);
+return (j);
 
 }
