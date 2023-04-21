516814b2612fce5e4be78ba9edcd9a04a7b2515e
Ella <ellalearnscopy@gmail.com>
2023-04-21 20:15:36 +0100

    drewwe

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function
+ * @h: argument
+ * Return: size_t number of elements
+*/
+size_t list_len(const list_t *h)
+{
+    size_t counter;
+    counter = 0;
+
+    if (!h)
+    {
+        return (counter);
+    }
+
+    while (h)
+    {
+        counter++;
+        h = h->next;
+    }
+
+    return (counter);
+
+}
