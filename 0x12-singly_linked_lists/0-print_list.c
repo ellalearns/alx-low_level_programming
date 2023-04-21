62a315a9caa9b53fe202407284bb218b0f5e357d
Ella <ellalearnscopy@gmail.com>
2023-04-21 20:11:15 +0100

    drew

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
  * print_list - function 
@@ -10,22 +8,22 @@
 */
 size_t print_list(const list_t *h)
 {
-    size_t counter = 0;
+size_t counter = 0;
 
-    while (h)
-    {
-        if (h->str)
-        {
-            printf("[%u] %s\n", h->len, h->str);
-            h = h->next;
-            counter++;
-        }
-        else
-        {
-            printf("[0] (nil)\n");
-        }
-    }
+while (h)
+{
+if (h->str)
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+counter++;
+}
+else
+{
+printf("[0] (nil)\n");
+}
+}
 
-    return (counter);
+return (counter);
 
 }
