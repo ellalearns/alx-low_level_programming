4e7fa35e712abda410f5c9b22c65183112096efa
Ella <ellalearnscopy@gmail.com>
2023-04-21 21:02:57 +0100

    erewer

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - function
+ * @head: argument
+ * @str: argument
+ * Return: list_t
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *tempNode;
+
+    tempNode = malloc(sizeof(list_t));
+
+    if (tempNode == NULL)
+    {
+        return (NULL);
+    }
+
+    tempNode->next = head;
+    tempNode->str = stdrup(str);
+    tempNode->len = strlen(str);
+
+    head = tempNode;
+
+    return (head);
+}
