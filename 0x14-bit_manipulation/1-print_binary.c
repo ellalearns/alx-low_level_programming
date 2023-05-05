00e377498ea360a87ff364cacf451a82eb5c7b82
Ella <ellalearnscopy@gmail.com>
2023-05-05 03:42:06 +0100

    dfwer

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - function
+ * @n: argument
+ * Return: nil
+*/
+void print_binary(unsigned long int n)
+{
+    unsigned long int nowBit;
+    int counter, iterator;
+    counter = 0;
+    iterator = 0;
+
+    for (iterator = 70; iterator >= 0; iterator++)
+    {
+        nowBit = n >> iterator;
+
+        if (nowBit & 1)
+        {
+            printf("%d", 1);
+            counter++;
+        }
+        else if (counter != 0)
+        {
+            printf("%d", 0);
+        }
+    }
+
+    if (counter == 0)
+    {
+        printf("%d", 0);
+    }
+}
