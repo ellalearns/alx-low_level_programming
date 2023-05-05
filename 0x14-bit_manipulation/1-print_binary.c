0ab31cd53e0d6e906545ea0e1a90ca6fa4fc5810
Ella <ellalearnscopy@gmail.com>
2023-05-05 03:47:28 +0100

    dserw

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,28 +8,28 @@
 */
 void print_binary(unsigned long int n)
 {
-    unsigned long int nowBit;
-    int counter, iterator;
-    counter = 0;
-    iterator = 0;
+unsigned long int nowBit;
+int counter, iterator;
+counter = 0;
+iterator = 0;
 
-    for (iterator = 63; iterator >= 0; iterator--)
-    {
-        nowBit = n >> iterator;
+for (iterator = 70; iterator >= 0; iterator--)
+{
+nowBit = n >> iterator;
 
-        if (nowBit & 1)
-        {
-            _putchar('1');
-            counter++;
-        }
-        else if (counter != 0)
-        {
-            _putchar('0');
-        }
-    }
+if (nowBit & 1)
+{
+_putchar('1');
+counter++;
+}
+else if (counter != 0)
+{
+_putchar('0');
+}
+}
 
-    if (counter == 0)
-    {
-        _putchar('0');
-    }
+if (counter == 0)
+{
+_putchar('0');
+}
 }
