6b68e7074140836f6a2f29c1ee1c80e549d1a24f
Ella <ellalearnscopy@gmail.com>
2023-05-05 03:52:45 +0100

    drwerw

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - function
+ * @n: first arg
+ * @index: second arg
+ * Return: integer bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+int bitNum;
+
+if (index >= 64)
+{
+return (-1);
+}
+if (index < 0)
+{
+return (-1);
+}
+bitNum = (n << index) & 1;
+return (bitNum);
+}
