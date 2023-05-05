ee1c90cd82f8d938c059584d045ab42e23f1a77c
Ella <ellalearnscopy@gmail.com>
2023-05-05 04:22:39 +0100

    sdrwer

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - function
+ * @n: argument
+ * @m: next argument
+ * Return: unsigned integer
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned int counter;
+unsigned long int nowBit;
+unsigned long int nxm;
+int iterator;
+
+nxm = n ^ m;
+
+for (iterator = 60; iterator >= 0; iterator--)
+{
+nowBit = (nxm >> iterator);
+if (nowBit & 1)
+{
+counter++;
+}
+}
+
+return (counter);
+}
