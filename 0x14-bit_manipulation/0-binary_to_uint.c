9e61983155e0dd1a64b83df8bef404c36a96061c
Ella <ellalearnscopy@gmail.com>
2023-05-05 03:10:19 +0100

    dreter

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - function
+ * @b: first arg
+ * Return: unsigned int
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int decNum;
+int counter;
+
+if (!b)
+{
+return (0);
+}
+else
+{
+counter = 0;
+while(b[counter])
+{
+if (b[counter] < '0' || b[counter] > '1')
+{
+return (0);
+}
+else
+{
+decNum = (2 * decNum) + (b[counter] - '0');
+}
+}
+}
+return (decNum);
+}
