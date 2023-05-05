2b4e2036b3973f9ae8b81a2c771f0151430b6e9f
Ella <ellalearnscopy@gmail.com>
2023-05-05 04:29:14 +0100

    whateber

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - function
+ * Return: integer
+*/
+int get_endianness(void)
+{
+char *str;
+unsigned int iterator;
+
+iterator = 1;
+*str = (char *) & iterator;
+
+return (*str);
+}
