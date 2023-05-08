bde1cdbc784540f3f12a5bf54020b2ea56d82c64
Ella <ellalearnscopy@gmail.com>
2023-05-08 19:22:17 +0100

    sdrewr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-
+#include "main.h"
 /**
  * create_file - function
  * @filename: argument
@@ -10,46 +10,29 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-    int fileToCreate;
-    int writeText;
-    int strlen;
-
-    if (filename == NULL)
-    {
-        return (-1);
-    }
-
-    if (text_content != NULL)
-    {
-        strlen = 0;
-        while (text_content[strlen])
-        {
-            strlen++;
-        }
-    }
-
-    fileToCreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-    if (fileToCreate < 0)
-    {
-        return (-1);
-    }
-
-    if (text_content == NULL)
-    {
-        return (1);
-    }
-
-    writeText = write(fileToCreate, text_content, (strlen));
-
-    if (writeText < 0)
-    {
-        return (-1);
-    }
-
-    close(fileToCreate);
-
-    return (1);
-
-
+int fileToCreate;
+int writeText;
+int strlen;
+if (filename == NULL)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+strlen = 0;
+while (text_content[strlen])
+{
+strlen++;
+}
+}
+fileToCreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fileToCreate < 0)
+return (-1);
+if (text_content == NULL)
+return (1);
+writeText = write(fileToCreate, text_content, (strlen));
+if (writeText < 0)
+return (-1);
+close(fileToCreate);
+return (1);
 }
