71376aae785c5f39849dfff40c1b0430ab452a26
Ella <ellalearnscopy@gmail.com>
2023-05-08 14:43:58 +0100

    dsrewr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
         return (-1);
     }
 
-    fileToCreate = open(filename, O_CREAT | O_RDWR);
+    fileToCreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
     if (fileToCreate < 0)
     {
