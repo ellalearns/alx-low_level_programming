4d05bb3d65beac0757b32967306ae6ff65425c95
Ella <ellalearnscopy@gmail.com>
2023-05-08 14:50:29 +0100

    sdrewre

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,21 @@ int create_file(const char *filename, char *text_content)
 {
     int fileToCreate;
     int writeText;
+    int strlen;
 
     if (filename == NULL)
     {
         return (-1);
     }
 
+    if (text_content != NULL)
+    {
+        strlen = 0;
+        while (text_content[strlen])
+        {
+            strlen++;
+        }
+    }
     fileToCreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
     if (fileToCreate < 0)
@@ -30,7 +39,12 @@ int create_file(const char *filename, char *text_content)
         return (1);
     }
 
-    writeText = write(fileToCreate, text_content, 100);
+    writeText = write(fileToCreate, text_content, strlen);
+
+    if (writeText < 0)
+    {
+        return (-1);
+    }
 
     close(fileToCreate);
 
