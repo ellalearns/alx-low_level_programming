095a8ac547865e7a51174437dc4118dabcb7dbaf
Ella <ellalearnscopy@gmail.com>
2023-05-08 14:53:19 +0100

    dsrere

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content == NULL)
     {
-        return (-1);
+        return (1);
     }
 
     writeText = write(fileToCreate, text_content, strlen);
