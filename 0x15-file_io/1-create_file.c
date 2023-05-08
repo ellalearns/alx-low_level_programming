9d882ae5d7e9b19c77a968a94cce5dd15e3d80bc
Ella <ellalearnscopy@gmail.com>
2023-05-08 14:51:50 +0100

    sdtrerr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,8 @@ int create_file(const char *filename, char *text_content)
         {
             strlen++;
         }
-    }
+    }.
+
     fileToCreate = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
     if (fileToCreate < 0)
@@ -36,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content == NULL)
     {
-        return (1);
+        return (-1);
     }
 
     writeText = write(fileToCreate, text_content, strlen);
