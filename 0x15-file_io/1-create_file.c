8a4e0da3ce1247978c5740e3dabe3985ccf38067
Ella <ellalearnscopy@gmail.com>
2023-05-08 14:42:15 +0100

    drewresew3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
         return (-1);
     }
 
-    fileToCreate = open(filename, O_RDWR | O_CREAT);
+    fileToCreate = open(filename, O_CREAT | O_RDWR);
 
     if (fileToCreate < 0)
     {
