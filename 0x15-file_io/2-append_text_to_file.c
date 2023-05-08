d9d4655aae53c469e82c9aed8d64dcccdf6843d3
Ella <ellalearnscopy@gmail.com>
2023-05-08 19:09:58 +0100

    sdarew

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,19 +14,15 @@ int append_text_to_file(const char *filename, char *text_content)
 int theFile;
 int appendText;
 int textLength;
-
 if (filename == NULL)
 {
 return (-1);
 }
-
 theFile = open(filename, O_APPEND | O_WRONLY | O_CREAT);
-
 if (theFile < 0)
 {
 return (-1);
 }
-
 if (text_content != NULL)
 {
 textLength = 0;
@@ -35,16 +31,11 @@ while (text_content[textLength])
 textLength++;
 }
 }
-
 appendText = write(theFile, text_content, textLength);
-
 if (appendText < 1)
 {
 return (-1);
 }
-
 close(theFile);
-
 return (1);
-
 }
