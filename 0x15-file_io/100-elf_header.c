573e2d6e86287cb5c0a39b51a731bc1e66c0f57b
Ella <ellalearnscopy@gmail.com>
2023-05-09 03:23:02 +0100

    sdrer77

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+    printf("%d", argc);
+    printf("%s", argv[1]);
+
+
+    return (0);
+}
