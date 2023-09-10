d6319fda0acdc144db3d73db44699b89264a59ce
Ella <ellalearnscopy@gmail.com>
2023-09-10 11:07:39 +0100

    refactor:replaced calloc with malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ return (NULL);
 }
 
 new_table->size = size;
-new_table->array = calloc(new_table->size, sizeof(hash_node_t));
+new_table->array = malloc(sizeof(hash_node_t *) * size);
 if (new_table->array == NULL)
 {
 return (NULL);
