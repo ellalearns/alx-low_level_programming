7977f5985793dce7a8272e0c46635a4370e78944
Ella <ellalearnscopy@gmail.com>
2023-09-10 11:04:52 +0100

    refactor:betty style mods

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,33 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a new hash table
- * @size: the size of the hash table
- * Return: new hash table
- */
+* hash_table_create - creates a new hash table
+* @size: the size of the hash table
+* Return: new hash table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t* new_table;
-    unsigned long int currentNode = 0;
+hash_table_t *new_table;
+unsigned long int currentNode = 0;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-    {
-        return (NULL);
-    }
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+{
+return (NULL);
+}
 
-    new_table->size = size;
-    new_table->array = calloc(new_table->size, sizeof(hash_node_t));
-    if (new_table->array == NULL)
-    {
-        return (NULL);
-    }
+new_table->size = size;
+new_table->array = calloc(new_table->size, sizeof(hash_node_t));
+if (new_table->array == NULL)
+{
+return (NULL);
+}
 
-    while (currentNode < new_table->size)
-    {
-        new_table->array[currentNode] = NULL;
-        currentNode++;
-    }
+while (currentNode < new_table->size)
+{
+new_table->array[currentNode] = NULL;
+currentNode++;
+}
 
-    return (new_table);
+return (new_table);
 }
