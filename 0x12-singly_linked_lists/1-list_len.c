a65180fc3bada23b3be80be7bd06c792c1ca6f6d
ellalearns <ellalearnscopy@gmail.com>
2024-01-07 02:24:40 -0800

    task 1 - number of elements in a singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - return the number of elements in a singly linked list
+ * @h: the linked list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t number_of_elements;
+	number_of_elements = 0;
+
+	while (h)
+	{
+		number_of_elements++;
+		h = h->next;
+	}
+
+	return (number_of_elements);
+}
