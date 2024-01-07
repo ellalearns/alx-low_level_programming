384cfaddbcf20921f8b1453132ea27aed6952d46
ellalearns <ellalearnscopy@gmail.com>
2024-01-07 02:01:40 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print the elements of a singly linked list
+ * @h: the singly linked list
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t number_of_nodes;
+
+	number_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			number_of_nodes++;
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+	}
+
+	return (number_of_nodes);
+}
