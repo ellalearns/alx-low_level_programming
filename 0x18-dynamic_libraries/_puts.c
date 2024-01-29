6d5c3eecccce2275c2bf63befadff6bb02bd33c4
ellalearns <ellalearnscopy@gmail.com>
2024-01-30 00:06:03 +0100

    task 0

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* _putchar - the func
+* @c: arg
+* Return: 0
+*/
+void _puts(char *s) {
+
+}
---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* _putchar - the func
+* @c: arg
+* Return: 0
+*/
+int _strlen(char *s) {
+return (0);
+}
