a34cf6e50c51ee7f363c7b88109e167a58f9e200
ellalearns <ellalearnscopy@gmail.com>
2024-01-30 00:19:21 +0100

    c files

---
+++ 0x18-dynamic_libraries/_isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _putchar - the func
+* @c: arg
+* Return: 0
+*/
+int _isalpha(int c) {
+
+    return (0);
+}
---
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* _putchar - the func
+* @c: arg
+* Return: 0
+*/
+char *_strcpy(char *dest, char *src){
+    return ("");
+}
