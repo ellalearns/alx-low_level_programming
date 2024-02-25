6f2e40dfcd85d0b9ff2d8b43719ddb46cc4f217b
Ella <ellalearnscopy@gmail.com>
2024-02-25 09:31:36 +0100

    fix betty style errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,27 +7,27 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    unsigned long int counter;
+hash_table_t *hash_table;
+unsigned long int counter;
 
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-    {
-        return (NULL);
-    }
+hash_table = malloc(sizeof(hash_table_t));
+if (hash_table == NULL)
+{
+return (NULL);
+}
 
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t) * size);
-    if (hash_table->array == NULL)
-    {
-        return (NULL);
-    }
-    while (counter < hash_table->size)
-    {
-        hash_table->array[counter] = NULL;
-        counter++;
-    }
+hash_table->size = size;
+hash_table->array = malloc(sizeof(hash_node_t) * size);
+if (hash_table->array == NULL)
+{
+return (NULL);
+}
+while (counter < hash_table->size)
+{
+hash_table->array[counter] = NULL;
+counter++;
+}
 
-    return (hash_table);
+return (hash_table);
 
 }
