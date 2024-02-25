604e3b084f61edb0c5e8f897823ec755e88bac93
Ella <ellalearnscopy@gmail.com>
2024-02-25 09:29:44 +0100

    replace calloc with malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     }
 
     hash_table->size = size;
-    hash_table->array = calloc(hash_table->size, sizeof(hash_node_t));
+    hash_table->array = malloc(sizeof(hash_node_t) * size);
     if (hash_table->array == NULL)
     {
         return (NULL);
