25f8513405d84cb90bc5a8848dcba4a539f6868d
ellalearns <ellalearnscopy@gmail.com>
2024-05-22 22:30:43 +0100

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search a list linearly
+ * @array: pointer to the first element of list
+ * @size: size of the list
+ * @value: the value to search for
+ * Return: first index where value is found or -1 if fail
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t counter = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (counter < size)
+	{
+		printf("%d", array[counter]);
+		if (array[counter] == value)
+			return (counter);
+		counter++;
+	}
+
+	return (-1);
+}
